4930b7009883cc893b6fa7fbc876e78a80f87931
Valentin Dubaleko <dubaleco@yandex.ru>
2019-04-21 00:12:41 +0300

    Little change in 8 lab

--- laba8/laba8/MainWindow.xaml.cs
+++ laba8/laba8/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,9 +22,15 @@ namespace laba8
     /// </summary>
     public partial class MainWindow : Window
     {
+        string connectionString;
         public MainWindow()
         {
             InitializeComponent();
+            string path = @"D:\Labs\2 sem\OOP\laba8\laba8\bin\Debug\App.config";
+            using (StreamReader sr = new StreamReader(path))
+            {
+              connectionString = sr.ReadToEnd();
+            }
         }
 
         private void clear()
@@ -44,19 +51,22 @@ namespace laba8
             }
         }
 
-        string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=OOP;Integrated Security=True";
-
         private void AddData_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-            string sqlExpression = "INSERT INTO Student (ФИО, Возраст, Дата_Рождения,Специальность,Пол,Курс,Группа) VALUES ('" + FIO.Text + "'," + Convert.ToInt32(Age.Text) + ",'" + Convert.ToDateTime(DataPicker.Text) + "', '" + Specialization.Text + "', '" + Gender.Text + "', " + Convert.ToInt32(Course.Text) + "," + Convert.ToInt32(Group.Text) + ")" +
-                "INSERT INTO Adress(Полное_Имя_Жильца,Город,Улица,Индекс,Дом ,Квартира) VALUES ('"+FIO.Text+"', '"+City.Text+"', '"+Street.Text+"', '"+Index.Text+"', "+Convert.ToInt32(Home.Text)+", "+Convert.ToInt32(Flat.Text)+")";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    SqlCommand command = new SqlCommand(sqlExpression, connection);
+                    SqlTransaction transaction = connection.BeginTransaction();
+                    SqlCommand command = connection.CreateCommand();
+                    command.Transaction = transaction;
+
+                    command.CommandText = "INSERT INTO Student (ФИО, Возраст, Дата_Рождения,Специальность,Пол,Курс,Группа) VALUES ('" + FIO.Text + "'," + Convert.ToInt32(Age.Text) + ",'" + Convert.ToDateTime(DataPicker.Text) + "', '" + Specialization.Text + "', '" + Gender.Text + "', " + Convert.ToInt32(Course.Text) + "," + Convert.ToInt32(Group.Text) + ")";
                     command.ExecuteNonQuery();
+                    command.CommandText = "INSERT INTO Adress(Полное_Имя_Жильца,Город,Улица,Индекс,Дом ,Квартира) VALUES ('" + FIO.Text + "', '" + City.Text + "', '" + Street.Text + "', '" + Index.Text + "', " + Convert.ToInt32(Home.Text) + ", " + Convert.ToInt32(Flat.Text) + ")";
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
                     MessageBox.Show("Данные добавлены", "Добавление данных", MessageBoxButton.OK, MessageBoxImage.Information);
                 }
                 clear();
@@ -71,13 +81,17 @@ namespace laba8
         {
            try
             {
-                string sqlExpression = "update Adress set  Город='"+City.Text+"', Улица='"+Street.Text+"' , Индекс='"+Index.Text+"', Дом = "+ Convert.ToInt32(Home.Text) + " , Квартира = "+ Convert.ToInt32(Flat.Text) + " where Полное_Имя_Жильца = '"+FIO.Text+"'" +
-                    "update Student set Возраст = "+ Convert.ToInt32(Age.Text) + ", Дата_рождения = '" + Convert.ToDateTime(DataPicker.Text) + "', Специальность = '" + Specialization.Text + "', Пол = '"+ Gender.Text+ "', Курс = " + Convert.ToInt32(Course.Text) + ", Группа = " + Convert.ToInt32(Group.Text) + " where ФИО = '" + FIO.Text+"'";
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    SqlCommand command = new SqlCommand(sqlExpression, connection);
+                    SqlTransaction transaction = connection.BeginTransaction();
+
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandText = "update Adress set  Город='" + City.Text + "', Улица='" + Street.Text + "' , Индекс='" + Index.Text + "', Дом = " + Convert.ToInt32(Home.Text) + " , Квартира = " + Convert.ToInt32(Flat.Text) + " where Полное_Имя_Жильца = '" + FIO.Text + "'";
                     command.ExecuteNonQuery();
+                    command.CommandText = "update Student set Возраст = " + Convert.ToInt32(Age.Text) + ", Дата_рождения = '" + Convert.ToDateTime(DataPicker.Text) + "', Специальность = '" + Specialization.Text + "', Пол = '" + Gender.Text + "', Курс = " + Convert.ToInt32(Course.Text) + ", Группа = " + Convert.ToInt32(Group.Text) + " where ФИО = '" + FIO.Text + "'";
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
                     MessageBox.Show("Данные обновлены", "Обновление данных", MessageBoxButton.OK, MessageBoxImage.Information);
                 }
                 clear();
--- laba8/laba8/Remove.xaml.cs
+++ laba8/laba8/Remove.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,23 +21,34 @@ namespace laba8
     /// </summary>
     public partial class Remove : Window
     {
+        string connectionString;
         public Remove()
         {
             InitializeComponent();
+            string path = @"D:\Labs\2 sem\OOP\laba8\laba8\bin\Debug\App.config";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                connectionString = sr.ReadToEnd();
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (string.IsNullOrEmpty(FIO.Text))
                 MessageBox.Show($"Ошибка: Заполните поле", "Error Message", MessageBoxButton.OK, MessageBoxImage.Error);
-            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=OOP;Integrated Security=True";
-            string sqlExpression = "DELETE FROM Adress where Полное_Имя_Жильца = '" + FIO.Text + "' DELETE FROM Student where ФИО='" + FIO.Text+"'";
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 connection.Open();
-                SqlCommand command = new SqlCommand(sqlExpression, connection);
-                int ss = command.ExecuteNonQuery();
-                MessageBox.Show(ss.ToString());
+                SqlTransaction transaction = connection.BeginTransaction();
+                SqlCommand command = connection.CreateCommand();
+                command.Transaction = transaction;
+
+                command.CommandText="DELETE FROM Adress where Полное_Имя_Жильца = '" + FIO.Text + "'";
+                command.ExecuteNonQuery();
+                command.CommandText = "DELETE FROM Student where ФИО='" + FIO.Text + "'";
+                command.ExecuteNonQuery();
+
+                transaction.Commit();
                 MessageBox.Show("Данные удалены", "Удаление данных", MessageBoxButton.OK, MessageBoxImage.Information);
             }
             FIO.Text = "";
